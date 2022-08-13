f07c288aa3fc37582cb4e4b037a0e64b6e51c176
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-08-12 21:43:46 -0400

    ADDED INPUT + STDLIB

--- standard_libs.js
+++ standard_libs.js
@@ -8,7 +8,23 @@ mov eax, 4
 int 0x80      
 push edi
 ret
-}`
+}
+
+function input(){
+    mov eax, 3
+    mov ebx, 0  
+    mov ecx, esi 
+    mov edx, 2
+    int 0x80
+    pop esi
+    ret
+}`,
+['Stdlib']: `
+SYS_EXIT  equ 1
+SYS_READ  equ 3
+SYS_WRITE equ 4
+STDIN     equ 0
+STDOUT    equ 1`
 }
 
 export default libs;
