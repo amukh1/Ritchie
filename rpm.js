2020f2464ee1e8e836484ded9713943eadf82fa8
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-08-19 13:47:00 -0400

    Version 1.1.28! Alpha RPM

--- rpm.js
+++ rpm.js
@@ -3,10 +3,51 @@ import fs from 'fs'
 // import { LocalStorage } from "node-localstorage";
 import inquirer from 'inquirer'
 import fetch from 'node-fetch';
+import axios from 'axios'
 
 
 if(process.argv[2] == 'install'){
     console.log(`installing ${process.argv[3]}..`)
+// let x =await fetch(`https://RPM.amukh1.repl.co/newPackageDownload?name=${process.argv[3]}`) // add download to package
+// let y = await x.text()
+// console.log(y)
+
+//     let res = await fetch(`https://RPM.amukh1.repl.co/getPackage?name=${process.argv[3]}`)
+//     let data = await res.text()
+//     console.log(data)
+
+// send get req to `https://RPM.amukh1.repl.co/newPackageDownload?name=${process.argv[3]}` with axios
+
+    axios.get(`https://RPM.amukh1.repl.co/newPackageDownload?name=${process.argv[3]}`).then(res => {
+        console.log(res.data)
+    })
+
+    // send get req to `https://RPM.amukh1.repl.co/getPackage?name=${process.argv[3]}` with axios
+
+    axios.get(`https://RPM.amukh1.repl.co/getPackage?name=${process.argv[3]}`).then(res => {
+        // console.log(res.data)
+        let dta = res.data
+        dta.forEach(file => {
+    
+            // console.log(`${Object.keys(file)[0]}: ${file[Object.keys(file)[0]]}`)
+            let fileName = Object.keys(file)[0]
+            console.log(`Downloading.. ${fileName}`)
+            let fileData = file[Object.keys(file)[0]]
+            if(fs.existsSync(`./${process.argv[3]}`)){
+                fs.writeFileSync(`./${process.argv[3]}/${fileName}`, fileData, (err) => {
+                    if (err) throw err;
+                })
+            }else{
+            fs.mkdirSync(`./${process.argv[3]}`)
+            fs.writeFileSync(`./${process.argv[3]}/${fileName}`, fileData, (err) => {
+                if (err) throw err;
+            })
+            }
+            
+            
+        });
+    })
+
 }else if(process.argv[2] == 'package'){
     console.log(`packaging..`)
     
@@ -36,6 +77,7 @@ if(process.argv[2] == 'install'){
         let res = await fetch(`https://RPM.amukh1.repl.co/userAuth?name=${a.username}&password=${a.password}`)
         let data = await res.text()
         if(data == 'Incorrect Password' || data == 'error'){
+            console.log(data)
         }else {
             console.log('Package successfully packaged under the name: ' + a.username)
             
@@ -85,7 +127,7 @@ if(process.argv[2] == 'install'){
         }else {
             console.log('Publishing package under the name: ' + a.username)
             // fetch `https://RPM.amukh1.repl.co/newPackage?name=${packager.name}&author=${packager.author}` with body {}
-            sob()
+            sob(packager, a)
         }
     })
 
@@ -172,7 +214,7 @@ async function signup(a) {
     }
 }
 
-let sob = () => {
+let sob = (packager, a) => {
     let resp = {}
     fs.readdir(`./`, (err, files) => {
         if (err) {
@@ -181,7 +223,7 @@ let sob = () => {
             // files = an array with all the file NAMES
             files.forEach(file => {
                 // console.log(file)
-                fs.readFile(`./${file}`, 'utf-8', (err, data) => {
+                fs.readFile(`./${file}`, 'utf-8', async (err, data) => {
                     if (err) {
                         console.log(err)
                     } else {
@@ -190,13 +232,18 @@ let sob = () => {
                         // console.log(data)
                         resp[file] = data
                         if(files.length == Object.keys(resp).length){
-                            console.log(resp)
-                            let res = await fetch(`https://RPM.amukh1.repl.co/newPackage?name=${packager.name}&author=${packager.author}`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    croc: 'croc',
-                    files: resp
-                })
+                            // console.log(resp)
+            //                 let res = await fetch(`https://RPM.amukh1.repl.co/newPackage?name=${packager.name}&author=${packager.author}`, {
+            //     method: 'POST',
+            //     body: {
+            //         "readme.md": "hello world!"
+            //     }
+            // })
+            // let data = await res.text()
+            // console.log(data)
+            // console.log(a)
+            axios.post(`https://RPM.amukh1.repl.co/newPackage?name=${packager.name}&author=${a.username}`, resp).then(res => {
+                console.log(res.data)
             })
                         }
                     }
@@ -208,3 +255,4 @@ let sob = () => {
 }
 
 // console.log(sob())
+
